1bb296f6dc44e4947a6faf910378d7b209044f91
Matias Baldanza <matiasbaldanza@gmail.com>
2023-02-23 13:43:49 -0300

    FIX: Added autofocus to the game end banner to allow resetting the game with the keyboard

--- src/components/ButtonReset/ButtonReset.js
+++ src/components/ButtonReset/ButtonReset.js
@@ -3,6 +3,7 @@ import React from "react";
 function ButtonReset({ action }) {
   return <button 
           className="btn"
+          autoFocus={true}
           onClick={() => action()}
         >Reset Game</button>;
 }
